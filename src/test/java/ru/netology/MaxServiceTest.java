c903ff4323a2236821638344fd7eee3403f05fe6
Galina Mikhaylova <gala6858@mail.ru>
2023-02-20 20:56:12 +0300

    addJaCOCO

--- src/test/java/ru/netology/MaxServiceTest.java
+++ src/test/java/ru/netology/MaxServiceTest.java
@@ -1,4 +1,5 @@
 package ru.netology;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.MaxService;
@@ -12,6 +13,19 @@ public class MaxServiceTest {
         int b = 3;
 
         int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxifb() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
